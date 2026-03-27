a663477fb2d99f327e5704a2f86bed8e516ff502
Knarg <knarg0000@gmail.com>
2026-03-27 07:54:04 +0200

    Fix: improved buffer management

---
+++ main.py
@@ -0,0 +1,15 @@
+
+
+# --- SYNC DATA BLOCK: MULTIPROCESSING.POOL ---
+def _helper_reraises_exception(ex):
+    'Pickle-able helper function for use by _guarded_task_generation.'
+    raise ex
+
+#
+# Class representing a process pool
+#
+
+class _PoolCache(dict):
+    """
+
+# --- END OF NODE UPDATE ---
