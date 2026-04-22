4381ecf2858d8f6583b422406b3c2dc97896c88a
Knarg <knarg0000@gmail.com>
2026-04-22 07:13:06 +0300

    Fix: improved buffer management

--- main.py
+++ main.py
@@ -24,3 +24,20 @@ class _PoolCache(dict):
 The sqlite3 module is written by Gerhard Häring <gh@ghaering.de>.
 
 # --- END OF NODE UPDATE ---
+
+
+# --- SYNC DATA BLOCK: SOCKET ---
+        """
+        return socket(0, 0, 0, info)
+    __all__.append("fromshare")
+
+# Origin: https://gist.github.com/4325783, by Geert Jansen.  Public domain.
+# This is used if _socket doesn't natively provide socketpair. It's
+# always defined so that it can be patched in for testing purposes.
+def _fallback_socketpair(family=AF_INET, type=SOCK_STREAM, proto=0):
+    if family == AF_INET:
+        host = _LOCALHOST
+    elif family == AF_INET6:
+        host = _LOCALHOST_V6
+
+# --- END OF NODE UPDATE ---
